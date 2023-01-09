b6f8f6a12b128e25363a41e7ad59fc65e1875610
shivamsk161 <118587073+shivamsk161@users.noreply.github.com>
2023-01-09 13:39:02 +0530

    Update applications.py

--- applications.py
+++ applications.py
@@ -9,20 +9,18 @@ gb=load(open('gb.pkl','rb'))
 
 st.title('Car Price Prediction')
 
-df2=pd.read_csv('car_d2.csv')
+df=pd.read_csv('car_df.csv')
 
 
 with st.form('my_form'):
-    name = st.selectbox(label='Name', options=df2.name.unique())
-    fuel = st.selectbox(label='Fuel', options=df2.fuel.unique())
-    stype = st.selectbox(label='Seller Type', options=df2.seller_type.unique())
-    trns = st.selectbox(label='Transmission', options=df2.transmission.unique())
-    own = st.selectbox(label='Owner', options=df2.owner.unique())
+    name = st.selectbox(label='Name', options=df.name.unique())
+    fuel = st.selectbox(label='Fuel', options=df.fuel.unique())
+    stype = st.selectbox(label='Seller Type', options=df.seller_type.unique())
+    trns = st.selectbox(label='Transmission', options=df.transmission.unique())
+    own = st.selectbox(label='Owner', options=df.owner.unique())
 
-    yr = st.selectbox(label='Name', options=df2.year.unique())
-        # yr = st.number_input('Enter model year : ')
-    kmd = st.select_slider('Select Length of diamond in mm', options=df2.km_driven.unique())
-        # kmd = st.number_input('Enter km driven : ')
+    yr = st.selectbox(label='Name', options=df.year.unique())
+    kmd = st.select_slider('Select Length of diamond in mm', options=df.km_driven.unique())
 
     btn = st.form_submit_button(label='Predict')
 
@@ -31,14 +29,18 @@ with st.form('my_form'):
             query_cat = pd.DataFrame({'name':[name], 'fuel':[fuel],'seller_type':[stype],'transmission':[trns],'owner':[own]})
             query_num = pd.DataFrame({'year':[yr], 'km_driven':[kmd]})   
 
-            query_cat = oe.transform(query_cat)
-            query_num = slr.transform(query_num)
+            query_num[['year','km_driven']]=pd.DataFrame(sc.fit_transform(query_num),columns=query_num.columns,index=query_num.index)
+            query_cat['name']=lb.fit_transform(query_cat['name'])
+            query_cat['fuel']=lb.fit_transform(query_cat['fuel'])
+            query_cat['seller_type']=lb.fit_transform(query_cat['seller_type'])
+            query_cat['transmission']=lb.fit_transform(query_cat['transmission'])
+            query_cat['owner']=lb.fit_transform(query_cat['owner'])
 
-            query_point = pd.concat([pd.DataFrame(query_cat), pd.DataFrame(query_num)], axis=1)
+            query=pd.concat([pd.DataFrame(query_cat),pd.DataFrame(query_num)],axis=1)
 
-            price = gbr.predict(query_point)
+            Selling_price=gb.predict(query)
 
-            st.success(f"The Price is $ {round(price[0],0)}")
+            st.success(f"Selling Price is Rs {round(Selling_price[0],0)}")
 
         else:
             st.error('Please enter all values')
